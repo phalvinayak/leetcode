9a2a5bd4ba5d8affa22cf321c1c2b7df37004a09
Vinayak Phal <phalvinayak@gmail.com>
2023-02-16 16:55:18 +0530

    Added runtime and space optimization

--- Assignment-02/01-CountPrime(204)/Solution.js
+++ Assignment-02/01-CountPrime(204)/Solution.js
@@ -3,29 +3,28 @@
  * @return {number}
  */
 var countPrimes = function (n) {
+  if (n <= 2) {
+    return 0;
+  }
   // Declaring array of flags as true
-  let flags = [...new Array(n)].map(() => true);
+  let flags = new Uint8Array(n);
   // Iterating till sqrt of 2
   // Starting from 2 prime number, mark its all multiple to false
   // as those multiiples are not prime which is obvious.
+  let count = n - 2; // Minus 2 to skip 0 and 1 index
   for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (flags[i]) {
+    if (!flags[i]) {
       // Iterate only if flag is set to true
       // We are finding all the multiple of prime till the given number
-      for (let j = i; i * j <= n; j++) {
-        flags[i * j] = false;
+      for (let j = i; i * j < n; j++) {
+        if (!flags[i * j]) {
+          count--;
+        }
+        flags[i * j] = 1;
       }
     }
   }
-
-  // Counting the flags which are true, by skipping the first 2 flags
-  let count = 0;
-  flags.forEach((m, i) => {
-    if (i > 1 && m) {
-      count++;
-    }
-  });
   return count;
 };
 
-console.log(countPrimes(10));
+console.log(countPrimes(20));
